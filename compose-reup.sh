16be9d97df3873213f34748bbd182265b938f277
murakami <murakami77@nifty.com>
2026-04-19 19:39:39 +0900

    OllamaのComposeのコンテナ起動と関係のないボリュームの扱いを削除

--- compose-reup.sh
+++ compose-reup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-echo "ディレクトリの権限を変更します"
-echo "- elasticsearch/esdata1"
-chmod 2777 elasticsearch/esdata1
-ls -l -a elasticsearch/
-echo "- solr/data"
-chmod 2777 solr/data
-ls -l -a solr/
-echo "- pgvector/data"
-chmod 777 pgvector/data
-ls -l -a pgvector/
-
 echo "downします"
 docker compose down
 
